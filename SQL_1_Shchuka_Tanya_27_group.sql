7492ddd1e68ca1a33ae28f18b49977568a5a23b7
Tanya <feniks7794@gmail.com>
2022-04-13 12:28:12 +0300

    add edit HW_1 SQL

--- SQL_1_Shchuka_Tanya_27_group.sql
+++ SQL_1_Shchuka_Tanya_27_group.sql
@@ -41,63 +41,63 @@ select name from students
 where name like '%a%'
 
 ---13. ¬ывести пользователей которые были созданы 2021-07-12 00:00:00
-select name from students
+select id, name, created_on from students
 where created_on = '2021-07-12 00:00:00'
 
 --- 14. ¬ывести пользователей которые были созданы 2021-07-12 00:00:00 и имеют пароль 1m313
-select name from students
-where created_on ='2021-07-12 00:00:00' and "password" = '1m313'
+select id, name, created_on, password  from students
+where created_on ='2021-07-12 00:00:00' and password = '1m313'
 
 --- 15. ¬ывести пользователей которые были созданы 2021-07-12 00:00:00 и у которых в имени есть слово Andrey
-select name from students
+select id, name, created_on from students
 where created_on = '2021-07-12 00:00:00' and name like '%Andrey%'
 
 ---16. ¬ывести пользователей которые были созданы 2021-07-12 00:00:00 и у которых в имени есть цифра 8
-select name from students
+select id, name, created_on from students
 where created_on = '2021-07-12 00:00:00' and name like '%8%'
 
 --- 17. ¬ывести пользовател€ у которых id равен 110
-select name from students
+select id, name from students
 where id = 110
 
 ---18. ¬ывести пользовател€ у которых id равен 153
-select name from students
+select id, name from students
 where id = 153
 
 ---19. ¬ывести пользовател€ у которых id больше 140
-select name from students
+select id, name from students
 where id > 140
 
  ---20. ¬ывести пользовател€ у которых id меньше 130
-select name from students
+select id, name from students
 where id < 130
 
 ---21. ¬ывести пользовател€ у которых id меньше 127 или больше 188
-select name from students
+select id, name from students
 where id < 127 or id > 188
 
 --- 22. ¬ывести пользовател€ у которых id меньше либо равно 137
-select name from students
+select id, name from students
 where id <= 137
 
 ---23. ¬ывести пользовател€ у которых id больше либо равно 137
-select name from students
+select id, name from students
 where id >= 137
 
 --- 24. ¬ывести пользовател€ у которых id больше 180 но меньше 190
-select name from students
+select id, name from students
 where id > 180 and id < 190
 
 --- 25. ¬ывести пользовател€ у которых id между 180 и 190
-select name from students
+select id, name from students
 where id between  180 and 190
 
 --- 26. ¬ывести пользователей где password равен 12333, 1m313, 123313
-select name from students
+select id, name, password from students
 where password in ('12333', '1m313', '123313')
 
 --- 27. ¬ывести пользователей где created_on равен 2020-10-03 00:00:00, 2021-05-19 00:00:00, 2021-03-26 00:00:00
-select name from students
+select id, name, created_on from students
 where created_on in ('2020-10-03 00:00:00', '2021-05-19 00:00:00', '2021-03-26 00:00:00')
 
 ---28. ¬ывести минимальный id 
@@ -107,7 +107,7 @@ select min(id) from students
 select max(id) from students
 
 ---30. ¬ывести количество пользователей
-select count(name) from students
+select count(id) from students
 
 --- 31. ¬ывести id пользовател€, им€, дату создани€ пользовател€.
 ---ќтсортировать по пор€дку возрастани€ даты добавлени€ пользоватлел€.
